98a97a94e1584f1f7fda6775b3f029ff75eb0d1f
Moya <b1h13322@htlf.com>
2021-07-01 11:20:26 -0500

    Edit loanForm.js to populate 'State' field dynamically

--- force-app/main/default/lwc/loanForm/loanForm.js
+++ force-app/main/default/lwc/loanForm/loanForm.js
@@ -1,15 +1,69 @@
-import { LightningElement } from 'lwc';
+import {LightningElement} from 'lwc';
 
 export default class LoanForm extends LightningElement {
     value = ['option1'];
+    states = ["AK",
+        "AL",
+        "AR",
+        "AS",
+        "AZ",
+        "CA",
+        "CO",
+        "CT",
+        "DC",
+        "DE",
+        "FL",
+        "GA",
+        "GU",
+        "HI",
+        "IA",
+        "ID",
+        "IL",
+        "IN",
+        "KS",
+        "KY",
+        "LA",
+        "MA",
+        "MD",
+        "ME",
+        "MI",
+        "MN",
+        "MO",
+        "MS",
+        "MT",
+        "NC",
+        "ND",
+        "NE",
+        "NH",
+        "NJ",
+        "NM",
+        "NV",
+        "NY",
+        "OH",
+        "OK",
+        "OR",
+        "PA",
+        "PR",
+        "RI",
+        "SC",
+        "SD",
+        "TN",
+        "TX",
+        "UT",
+        "VA",
+        "VI",
+        "VT",
+        "WA",
+        "WI",
+        "WV",
+        "WY"
+    ]
 
     get options() {
-        return [
-            { 
-                label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non augue leo. In feugiat nec odio at pretium. Quisque euismod non sapien eget efficitur.', 
-                value: 'option1' 
-            }
-        ];
+        return [{
+            label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non augue leo. In feugiat nec odio at pretium. Quisque euismod non sapien eget efficitur.',
+            value: 'option1'
+        }];
     }
 
     get selectedValues() {

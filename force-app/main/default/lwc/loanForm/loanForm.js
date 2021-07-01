6a665a9f0fd9c4e05fac59597df9d29bb1ed6ab1
Moya <b1h13322@htlf.com>
2021-07-01 09:57:59 -0500

    Add checkbox HTML, CSS and JS

--- force-app/main/default/lwc/loanForm/loanForm.js
+++ force-app/main/default/lwc/loanForm/loanForm.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
 
-export default class LoanForm extends LightningElement {}
+export default class LoanForm extends LightningElement {
+    value = ['option1'];
+
+    get options() {
+        return [
+            { 
+                label: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla non augue leo. In feugiat nec odio at pretium. Quisque euismod non sapien eget efficitur.', 
+                value: 'option1' 
+            }
+        ];
+    }
+
+    get selectedValues() {
+        return this.value.join(',');
+    }
+
+    handleChange(e) {
+        console.log(e);
+        this.value = e.detail.value;
+    }
+}

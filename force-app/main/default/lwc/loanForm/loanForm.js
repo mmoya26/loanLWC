251794b5f422584cab7df68e9973b1100db6c0d3
Moya <b1h13322@htlf.com>
2021-07-06 09:52:06 -0500

    Add social security number formatting fuction + Add comments to already existing functions

--- force-app/main/default/lwc/loanForm/loanForm.js
+++ force-app/main/default/lwc/loanForm/loanForm.js
@@ -2,6 +2,8 @@ import {LightningElement} from 'lwc';
 
 export default class LoanForm extends LightningElement {
     value = ['S'];
+
+    // List of all states for the State select field to be populated
     states = ["AK",
         "AL",
         "AR",
@@ -87,8 +89,7 @@ export default class LoanForm extends LightningElement {
         this.value = e.detail.value;
     }
 
-
-    // Will handle the logic when the preview icon is clicked
+    // This fuction will handle the logic when the preview icon is clicked
     previewClicked(e) {
         let element = this.template.querySelector('.ssn_input');
 
@@ -100,5 +101,17 @@ export default class LoanForm extends LightningElement {
             // If the input field type attribute is already "text" then set back the attribute to be "password"
             element.type = "password";
         }
+    }
+
+    // This function will handle the Social Securit Number formatting
+    // Whenever there is a change in the value of the input field, this function will be called
+    formatSocialSecurityNumber(e) {
+        let val = e.target.value.replace(/\D/g, '');
+        val = val.replace(/^(\d{3})/, '$1-');
+        val = val.replace(/-(\d{2})/, '-$1-');
+        val = val.replace(/(\d)-(\d{4}).*/, '$1-$2');
+
+        // Set the value of our e.target(input) to be equals to the correct val.
+        e.target.value = val;
     }
 }
